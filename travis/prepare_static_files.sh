0384d6b4bd73961d750c1a2d625c7fb0261ebedc
Matei <4560298+mateimicu@users.noreply.github.com>
2018-04-28 00:21:37 +0300

    Initial commit

---
+++ travis/prepare_static_files.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+echo "Deleting old publication"
+
+CNAME="openstack-slides.mateimicu.com"
+
+echo "get the themes"
+git submodule update
+
+rm -rf public
+
+echo "Generating site"
+/tmp/hugo
+
+echo "Save the static files."
+cp -r public /tmp/hugo-public
+
+echo "Clean directory"
+rm -rf *
+
+echo "Copy the static files"
+cp -r /tmp/hugo-public/* .
+
+echo "Add CNAME reference for gh-pages"
+echo "$CNAME" >> CNAME
