9b6164353340f0ee6c4aff94dc4baebfd856160c
Matei-Marius Micu <matei.micu@mambu.com>
2020-07-14 22:40:15 +0300

    Switch to github actions

--- travis/get_hugo.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Clone the Hugo binary
-
-pushd /tmp
-
-wget "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz"
-tar -zxvf "/tmp/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz"
-cp "/tmp/hugo_${HUGO_VERSION}_Linux-64bit/hugo" "/tmp/hugo"
-popd
